4367c8c621d86e589f4d2a2d3e11c3bfed966e3a
ARElton <ameliareneeelton@gmail.com>
2020-12-13 16:26:19 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+let dodger = document.getElementById("dodger"); 
+
+// LEFT
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+
+// RIGHT
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
